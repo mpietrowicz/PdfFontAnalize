00e6faec8d14e3c5e6e9c6dbadcc04523b9da903
Michał Pietrowicz <michal.pietrowicz@nask.pl>
2025-12-18 11:47:32 +0100

    Add unit tests for AsposeWorker and update Dockerfile for test execution

---
+++ AnalysisOfAspose.Tests/AsposeWorkerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace AnalysisOfAspose.Tests;
+
+public class AsposeWorkerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task ConvertToPdf_Success()
+    {
+        var _uut = new AsposeWorker();
+        _uut.SetOutputFolder(Directory.GetCurrentDirectory());
+        var data = await _uut.ConvertToPdf("DocxFiles/TestCzcionek.docx", "ConvertToPdf_Success");
+        ClassicAssert.IsTrue(File.Exists(data));
+    }
+}
